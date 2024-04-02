debd021c1b1ea7bf9afb53cc0e792c6a6c4eef1f
Srikar <48818698+srikar-kotra@users.noreply.github.com>
2024-04-01 20:48:56 -0500

    Update ActionBar.java
    Refactored actionbar.java by removing switch statements and introducing new conditional with polymorphism

--- JEdit-5.5.0-master/JEdit-5.5.0-master/org/gjt/sp/jedit/gui/ActionBar.java
+++ JEdit-5.5.0-master/JEdit-5.5.0-master/org/gjt/sp/jedit/gui/ActionBar.java
@@ -295,16 +295,22 @@ public class ActionBar extends JToolBar
 	} //}}}
 
 	//{{{ ActionTextField class
-	private class ActionTextField extends HistoryTextField
+	public class ActionTextField extends HistoryTextField
 	{
 		boolean repeat;
 		boolean nonDigit;
-
+                private Map<Integer, KeyEventProcessor> processorMap;
 		ActionTextField()
 		{
 			super("action");
 			setSelectAllOnFocus(true);
+			initializeProcessors();
 		}
+		private void initializeProcessors() {
+        processorMap = new HashMap<>();
+        processorMap.put(KeyEvent.KEY_TYPED, new KeyTypedProcessor());
+        processorMap.put(KeyEvent.KEY_PRESSED, new KeyPressedProcessor());
+    }
 
 		@Override
 		public boolean getFocusTraversalKeysEnabled()
@@ -313,85 +319,18 @@ public class ActionBar extends JToolBar
 		}
 
 		@Override
-		public void processKeyEvent(KeyEvent evt)
-		{
-			evt = KeyEventWorkaround.processKeyEvent(evt);
-			if(evt == null)
-				return;
+		public void processKeyEvent(KeyEvent evt) {
+        KeyEventProcessor processor = processorMap.get(evt.getID());
+        if (processor != null) {
+            processor.process(evt, this);
+        } else {
+            superProcessKeyEvent(evt);
+        }
+    }
 
-			switch(evt.getID())
-			{
-			case KeyEvent.KEY_TYPED:
-				char ch = evt.getKeyChar();
-				if(!nonDigit && Character.isDigit(ch))
-				{
-					super.processKeyEvent(evt);
-					repeat = true;
-					repeatCount = Integer.parseInt(action.getText());
-				}
-				else
-				{
-					nonDigit = true;
-					if(repeat)
-					{
-						passToView(evt);
-					}
-					else
-						super.processKeyEvent(evt);
-				}
-				break;
-			case KeyEvent.KEY_PRESSED:
-				int keyCode = evt.getKeyCode();
-				if(evt.isActionKey()
-					|| evt.isControlDown()
-					|| evt.isAltDown()
-					|| evt.isMetaDown()
-					|| keyCode == KeyEvent.VK_BACK_SPACE
-					|| keyCode == KeyEvent.VK_DELETE
-					|| keyCode == KeyEvent.VK_ENTER
-					|| keyCode == KeyEvent.VK_TAB
-					|| keyCode == KeyEvent.VK_ESCAPE)
-				{
-					nonDigit = true;
-					if(repeat)
-					{
-						passToView(evt);
-						break;
-					}
-					else if(keyCode == KeyEvent.VK_TAB)
-					{
-						complete(true);
-						evt.consume();
-					}
-					else if(keyCode == KeyEvent.VK_ESCAPE)
-					{
-						evt.consume();
-						if(popup != null)
-						{
-							popup.dispose();
-							popup = null;
-							action.requestFocus();
-						}
-						else
-						{
-							if(temp)
-								view.removeToolBar(ActionBar.this);
-							view.getEditPane().focusOnTextArea();
-						}
-						break;
-					}
-					else if((keyCode == KeyEvent.VK_UP
-						|| keyCode == KeyEvent.VK_DOWN)
-						&& popup != null)
-					{
-						popup.list.processKeyEvent(evt);
-						break;
-					}
-				}
-				super.processKeyEvent(evt);
-				break;
-			}
-		}
+    void superProcessKeyEvent(KeyEvent evt) {
+        super.processKeyEvent(evt);
+    }
 
 		private void passToView(final KeyEvent evt)
 		{
